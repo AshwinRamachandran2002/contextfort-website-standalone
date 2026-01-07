ffb92ed73650bf52547c6dc60ce321f9b5c897fd
Ashwin Ramachandran <ashwin@Ashwins-MacBook-Air.local>
2026-01-06 17:19:29 -0800

    Auto-sync after agent response

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,33 +1,6 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": [
-      "static/chunks/be941_next_dist_compiled_next-devtools_index_2290a98e.js",
-      "static/chunks/be941_next_dist_compiled_a2d9fde3._.js",
-      "static/chunks/be941_next_dist_shared_lib_d931bcad._.js",
-      "static/chunks/be941_next_dist_client_41fdb3cb._.js",
-      "static/chunks/be941_next_dist_93f15f14._.js",
-      "static/chunks/be941_next_app_296ff8ba.js",
-      "static/chunks/[next]_entry_page-loader_ts_4f599389._.js",
-      "static/chunks/be941_react-dom_3f964d30._.js",
-      "static/chunks/be941_46907642._.js",
-      "static/chunks/[root-of-the-server]__529d70dc._.js",
-      "static/chunks/orchids-projects_contextfort-website_pages__app_2da965e7._.js",
-      "static/chunks/turbopack-orchids-projects_contextfort-website_pages__app_6534de9b._.js"
-    ],
-    "/_error": [
-      "static/chunks/be941_next_dist_compiled_next-devtools_index_2290a98e.js",
-      "static/chunks/be941_next_dist_compiled_a2d9fde3._.js",
-      "static/chunks/be941_next_dist_shared_lib_749a8d47._.js",
-      "static/chunks/be941_next_dist_client_41fdb3cb._.js",
-      "static/chunks/be941_next_dist_e1e80b98._.js",
-      "static/chunks/be941_next_error_fb622e3f.js",
-      "static/chunks/[next]_entry_page-loader_ts_5a1ccffc._.js",
-      "static/chunks/be941_react-dom_3f964d30._.js",
-      "static/chunks/be941_46907642._.js",
-      "static/chunks/[root-of-the-server]__d746da9b._.js",
-      "static/chunks/orchids-projects_contextfort-website_pages__error_2da965e7._.js",
-      "static/chunks/turbopack-orchids-projects_contextfort-website_pages__error_ac85e328._.js"
-    ]
+    "/_app": []
   },
   "devFiles": [],
   "ampDevFiles": [],
--- .next/server/pages/_app.js
+++
@@ -1,4 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__22e5ff09._.js")
-R.m("[project]/orchids-projects/contextfort-website/node_modules/next/app.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/orchids-projects/contextfort-website/node_modules/next/app.js [ssr] (ecmascript)").exports
